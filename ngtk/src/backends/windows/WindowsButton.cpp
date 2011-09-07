efceb94f47c2c9a3f6adbaa139ea6a3de97160a8
Barak Itkin <lightningismyname@gmail.com>
2011-09-07 11:00:28 +0000

    Add a Push Button widget

---
+++ ngtk/src/backends/windows/WindowsButton.cpp
@@ -0,0 +1,67 @@
+#include <Windows.h>
+#include <Commctrl.h>
+#include <Windowsx.h>
+
+#include "WindowsBackend.h"
+#include "WindowsWidget.h"
+#include "WindowsButton.h"
+
+namespace Ngtk
+{
+  namespace Backends
+  {
+    namespace Windows
+    {
+
+      LRESULT CALLBACK
+      WindowsButton::WndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+      {
+        return WindowsWidget::WndProc (hwnd, msg, wParam, lParam);
+      }
+
+      NGTK_DLL_FUNC
+      WindowsButton::WindowsButton (WindowsContainer *parent, std::string text)
+      : Base::AbstractWidget (text, true), WindowsComponent (parent)
+      {
+        WindowsComponent* p = dynamic_cast<WindowsComponent*> (parent);
+        this->hwnd = CreateWindowExA (
+                                      0, /* Default */
+                                      "BUTTON",
+                                      text.c_str (),
+									  WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON | BS_TEXT, /* Regular push button */
+                                      0, 0, /* X, Y */
+                                      50, 50, /* Temporary untill someone will change it */
+                                      p == NULL ? NULL : p->GetHwnd (), /* Parent window */
+                                      NULL, /* Number ID */
+                                      WindowsBackend::hinstance, /* Instance */
+                                      NULL /* The lParam passed with WM_CREATE message */
+                                      );
+
+        SetToHwnd (this->hwnd, const_cast<WindowsButton*> (this));
+		parent->RegisterChild (this);
+      }
+
+      NGTK_DLL_FUNC
+      WindowsButton::~WindowsButton () { }
+
+      bool
+      WindowsButton::NaturalMouseClickHandler (Events::MouseEvent &evnt, bool external)
+      {
+        return false;
+      }
+
+      void
+      WindowsButton::SetEnabled (bool enabled)
+      {
+        Button_Enable (this->hwnd, enabled);
+		AbstractWidget::SetEnabled (enabled);
+      }
+
+	  void
+	  WindowsButton::SetText (std::string text)
+	  {
+		  SendMessage (this->hwnd, WM_SETTEXT, NULL, (LPARAM) text.c_str ());
+	  }
+    };
+  }
+}
---
+++ ngtk/src/backends/windows/WindowsButton.h
@@ -0,0 +1,38 @@
+#ifndef __Ngtk_Backends_Windows_WindowsButton_h__
+#define __Ngtk_Backends_Windows_WindowsButton_h__
+
+#include <Windows.h>
+#include <string>
+
+#include "../base/AbstractWidget.h"
+#include "../base/AbstractWindow.h"
+#include "../../utils/DynamicLibrary.h"
+#include "WindowsContainer.h"
+#include "WindowsComponent.h"
+
+namespace Ngtk
+{
+  namespace Backends
+  {
+    namespace Windows
+    {
+      class WindowsButton : public Base::AbstractWidget, public WindowsComponent
+      {
+      protected:
+        static LRESULT CALLBACK WndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+      public:
+        NGTK_DLL_FUNC WindowsButton (WindowsContainer *parent, std::string text);
+        NGTK_DLL_FUNC ~WindowsButton ();
+
+		void
+	  WindowsButton::SetText (std::string text);
+        virtual bool NaturalMouseClickHandler (Events::MouseEvent &evnt, bool external);
+        virtual void SetEnabled (bool enabled);
+      };
+    }
+  }
+}
+
+#endif
+
