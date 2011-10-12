fb016ba176cfde8a0f4f6420f0442a8f54e4ed4f
Barak Itkin <lightningismyname@gmail.com>
2011-10-12 17:32:40 +0200

    Add the calculator demo program

---
+++ ngtk-c/src-test/calculator.c
@@ -0,0 +1,240 @@
+/*
+ * calculator.c
+ * (C) Barak Itkin <lightningismyname at gmail dot com>, 2011
+ *
+ * This file is part of NGtk.
+ *
+ * NGtk is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * NGtk is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with NGtk.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "calculator.h"
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "../src/ngtk.h" /* For TRUE and FALSE, Assertions, etc. */
+
+static double previous_number = 0;
+
+static int has_math_error = FALSE;
+
+static char display_text_buf[MAX_NUMBER_LEN+2] = { '\0' };
+static char text_buf[MAX_NUMBER_LEN+1] = { '\0' };
+static int  has_minus = FALSE;
+static int  current_number_has_dot = 0;
+static int  text_buf_pos = 0;
+
+static MathOp current_op = NOP;
+
+void HandleIfError ()
+{
+	if (has_math_error)
+		ClearPressed ();
+}
+
+const char* GetNumberToDisplay ()
+{
+	if (has_math_error)
+		return "42 is definetly not the answer!";
+	else if (text_buf_pos == 0)
+		sprintf (display_text_buf, " 0");
+	else
+		sprintf (display_text_buf, "%c%s", (has_minus ? '-' : '+'), text_buf);
+	return display_text_buf;
+}
+
+void Optimize ()
+{
+	int i = 0, j;
+
+	if (has_math_error) return;
+
+	while (text_buf[i] == '0' && i < MAX_NUMBER_LEN)
+		i++;
+
+	if (current_number_has_dot)
+		i--; /* We need one zero before the dot */
+
+	if (i > 0)
+	{
+		for (j = 0; j < text_buf_pos - i; j++)
+			text_buf[j] = text_buf[j+i];
+		text_buf[text_buf_pos = j] = '\0';
+	}
+}
+
+/* Like Optimize, but also remove un-necessary information after the
+ * decimal point */
+void OptimizeResult ()
+{
+	if (has_math_error) return;
+
+	Optimize ();
+	if (current_number_has_dot)
+	{
+		while (text_buf[text_buf_pos-1] == '0')
+			text_buf[--text_buf_pos] = '\0';
+		if (text_buf[text_buf_pos-1] == '.')
+		{
+			text_buf[--text_buf_pos] = '\0';
+			current_number_has_dot = FALSE;
+		}
+	}
+}
+
+/* Remove trailing zeros, or zeros after decimal dot */
+/* Simulate a digit that was pressed */
+void DigitPressed (char c)
+{
+	HandleIfError ();
+	if (text_buf_pos < MAX_NUMBER_LEN && isdigit (c))
+	{
+		text_buf[text_buf_pos++] = c;
+		text_buf[text_buf_pos] = '\0';
+	}
+
+	Optimize ();
+}
+
+void DotPressed ()
+{
+	HandleIfError ();
+	if (current_number_has_dot)
+		return;
+
+	if (text_buf_pos == 0) /* If we have nothing currently */
+		text_buf[text_buf_pos++] = '0';
+
+	text_buf[text_buf_pos++] = '.';
+
+	text_buf[text_buf_pos] = '\0';
+	current_number_has_dot = TRUE;
+
+	Optimize ();
+}
+
+void ErasePressed ()
+{
+	HandleIfError ();
+	if (text_buf_pos > 0)
+	{
+		if (text_buf[--text_buf_pos])
+			current_number_has_dot = FALSE;
+		text_buf[text_buf_pos] = '\0';
+	}
+
+	Optimize ();
+}
+
+const char* GetOpToDisplay ()
+{
+	if (has_math_error)
+		return ":D ";
+
+	switch (current_op)
+	{
+	case NOP:
+		return "";
+	case ADD:
+		return "+";
+	case MUL:
+		return "*";
+	case SUB:
+		return "-";
+	case DIV:
+		return "/";
+	default:
+		ngtk_assert (FALSE);
+		return NULL; /* Shut up compiler warnings */
+	}
+}
+
+double GetCurrentNumber ()
+{
+	return atof (text_buf) * (has_minus ? -1 : 1);
+}
+
+void ComputePressed ()
+{
+	double result;
+	double current;
+	int i;
+
+	if (has_math_error) return;
+
+	current = GetCurrentNumber ();
+
+	switch (current_op)
+	{
+	case NOP:
+		return;
+	case ADD:
+		result = previous_number + current; break;
+	case SUB:
+		result = previous_number - current; break;
+	case MUL:
+		result = previous_number * current; break;
+	case DIV:
+		if (current == 0)
+		{
+			has_math_error = TRUE;
+			return;
+		}
+		result = previous_number / current; break;
+	default:
+		ngtk_assert (FALSE);
+		return; /* Shut up compiler warnings */
+	}
+
+	text_buf_pos = sprintf (text_buf, "%f", ABS (result));
+	current_number_has_dot = FALSE;
+	for (i = 0; i < text_buf_pos; i++)
+		if (current_number_has_dot = (text_buf[i] == '.'))
+			break;
+	has_minus = result < 0;
+
+	current_op = NOP;
+
+	OptimizeResult ();
+}
+
+void OpPressed (char op)
+{
+	if (has_math_error) return;
+	ngtk_assert (op == '+' || op == '-' || op == '*' || op == '/');
+	if (current_op == NOP)
+	{
+		previous_number = GetCurrentNumber ();
+		text_buf[text_buf_pos = 0] = '\0';
+		has_minus = FALSE;
+		current_number_has_dot = 0;
+	}
+	current_op = (MathOp) op;
+}
+
+void ToggleMinus ()
+{
+	if (has_math_error) return;
+	has_minus = ! has_minus;
+}
+
+void ClearPressed ()
+{
+	has_math_error = FALSE;
+	current_op = NOP;
+	text_buf[text_buf_pos = 0] = '\0';
+	has_minus = FALSE;
+	current_number_has_dot = 0;
+}
--- ngtk-c/src-test/main.c
+++ ngtk-c/src-test/main.c
@@ -19,55 +19,204 @@
  */
 
 #include "../src/ngtk.h"
+#include "../src/layouts/ngtk-grid-layout.h"
 
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
+#include "calculator.h"
 #define MAX_STR 4096
-void HideOnClick (NGtkComponent *comp, const char* signame, void *sigdata, void *lisdata)
+
+NGtkComponent *number, *op;
+
+/* Calculator Logic in Pseudo Code:
+ *
+ * On "Op":
+ * - If CurrentOp == NOP:
+ * -     Current = Prev
+ * -     Single Type
+ * - Else:
+ * - Compute 
+ * On "Compute":
+ * NUMBER:
+ * : "Op":
+ * : -> prev = current, current = 
+ * : 
+ * : "Digit"/"Erase" -> NUMBER1 (change number)
+ * : "Compute"       -> NUMBER1 (do nothing)
+ *
+ * NUMBER2:
+ * : "COMPUTE"
+ *
+ */
+
+void UpdateGUI ()
 {
-	NGtkMouseEvent *evnt = (NGtkMouseEvent*) sigdata;
-	if (evnt->type == NGTK_MET_CLICK)
-		ngtk_component_set_visible (comp, ! ngtk_component_get_visible (comp));
+	ngtk_component_set_text (number, GetNumberToDisplay ());
+	ngtk_component_set_text (op, GetOpToDisplay ());
 }
 
-void QuitOnPress (NGtkComponent *comp, const char* signame, void *sigdata, void *lisdata)
+void NumberClick (NGtkComponent *comp, const char* signame, void *sigdata, void *lisdata)
 {
-	NGtkMouseEvent *evnt = (NGtkMouseEvent*) sigdata;
-	ngtk_debug (ngtk_base_get_backend (comp), "Received mouse event signal!");
-	if (evnt->type == NGTK_MET_CLICK)
+	if (((NGtkMouseEvent*) sigdata)->type == NGTK_MET_CLICK)
+	{
+		DigitPressed (ngtk_component_get_text (comp) [0]);
+		UpdateGUI ();
+	}
+}
+
+void DotClick (NGtkComponent *comp, const char* signame, void *sigdata, void *lisdata)
+{
+	if (((NGtkMouseEvent*) sigdata)->type == NGTK_MET_CLICK)
+	{
+		DotPressed ();
+		UpdateGUI ();
+	}
+}
+
+void OpClick (NGtkComponent *comp, const char* signame, void *sigdata, void *lisdata)
+{
+	if (((NGtkMouseEvent*) sigdata)->type == NGTK_MET_CLICK)
+	{
+		OpPressed (ngtk_component_get_text (comp) [0]);
+		UpdateGUI ();
+	}
+}
+
+void EraseClick (NGtkComponent *comp, const char* signame, void *sigdata, void *lisdata)
+{
+	if (((NGtkMouseEvent*) sigdata)->type == NGTK_MET_CLICK)
+	{
+		ErasePressed ();
+		UpdateGUI ();
+	}
+}
+
+void ComputeClick (NGtkComponent *comp, const char* signame, void *sigdata, void *lisdata)
+{
+	if (((NGtkMouseEvent*) sigdata)->type == NGTK_MET_CLICK)
+		ComputePressed ();
+	UpdateGUI ();
+}
+
+void ClearClick (NGtkComponent *comp, const char* signame, void *sigdata, void *lisdata)
+{
+	if (((NGtkMouseEvent*) sigdata)->type == NGTK_MET_CLICK)
+		ClearPressed ();
+	UpdateGUI ();
+}
+
+void ToggleClick (NGtkComponent *comp, const char* signame, void *sigdata, void *lisdata)
+{
+	if (((NGtkMouseEvent*) sigdata)->type == NGTK_MET_CLICK)
+	{
+		ToggleMinus ();
+		UpdateGUI ();
+	}
+}
+
+void CommonKeyboardHandler (NGtkComponent *comp, const char* signame, void *sigdata, void *lisdata)
+{
+	NGtkKeyboardEventKey key = ((NGtkKeyboardEvent*)sigdata)->key;
+	int someUpdate = TRUE;
+	if (key == '-' || key == '+' || key == '*' || key == '/')
+		OpPressed ((char)key);
+	else if (key == '.')
+		DotPressed ();
+	else if (key == 'c' || key == 'C')
+		ClearPressed ();
+	else if (key == 'i' || key == 'I')
+		ToggleMinus ();
+	else if (key >= NGTK_KKEY_MIN_CHAR && key <= NGTK_KKEY_MAX_CHAR && isdigit ((char)key))
+		DigitPressed ((char)key);
+	else if (key == NGTK_KKEY_BACKSPACE)
+		ErasePressed ();
+	else if (key == NGTK_KKEY_ENTER)
+		ComputePressed ();
+	else
+		someUpdate = FALSE;
+
+	if (someUpdate)
+		UpdateGUI ();
+}
+
+void QuitClick (NGtkComponent *comp, const char* signame, void *sigdata, void *lisdata)
+{
+	if (((NGtkMouseEvent*) sigdata)->type == NGTK_MET_CLICK)
 		ngtk_backend_quit_main_loop (ngtk_base_get_backend (comp));
 }
 
-void SimulateLabel (NGtkComponent *comp, const char* signame, void *sigdata, void *lisdata)
-{
-	char *textbuf = (char *) lisdata;
-	int curlen = strlen (textbuf);
+typedef enum {
+	LABEL,
+	BUTTON,
+	TEXT_ENTRY
+} WidgetType;
 
-	NGtkKeyboardEvent *kevent = (NGtkKeyboardEvent*) sigdata;
-	NGtkKeyboardEventKey key = kevent->key;
+typedef struct {
+	const char          *text;
+	const NGtkRectangle  rect;
+	WidgetType           wt;
+	NGtkListener         mouse_handler;
+	NGtkListener         keyboard_handler;
+} WidgetSpecs;
 
-//	printf ("SimulateLabel\n");
-	if (curlen > 0 && key == NGTK_KKEY_BACKSPACE)
-		textbuf[curlen - 1] = '\0';
-	else if (curlen < MAX_STR)
-	{
-		char toAdd = '\0';
-		if (key == NGTK_KKEY_SPACE)
-			toAdd = ' ';
-		else if (NGTK_KKEY_MIN_CHAR < key && key < NGTK_KKEY_MAX_CHAR)
-			toAdd = key;
+/* We are going to create a calculator with the
+ * following layout:
+ *
+ *   Op     Number
+ * +---+---------------+
+ * |   |               |
+ * |---+---+---+---+---|
+ * | 1 | 2 | 3 | + | < |
+ * |---+---+---+---+---|
+ * | 4 | 5 | 6 | - | C |
+ * |---+---+---+---+---|
+ * | 7 | 8 | 9 | * |   |
+ * |---+---+---+---| = |
+ * |   0   | . | / |   |
+ * +-------+---+---+---+
+ * |       Quit        |
+ * +-------------------+
+ *
+ * This actually based on a grid which is 5x6 (width x height)
+ */
 
-		textbuf[curlen] = toAdd;
-		textbuf[curlen+1] = '\0';
-	}
+const static int GridWidth = 5, GridHeight = 6;
 
-	ngtk_component_set_text (comp, textbuf);
-}
+const static WidgetSpecs gui[] = {
+	{ "",  { 0, 0, 1, 1 }, LABEL, NULL, NULL },
+	{ "",  { 1, 0, 4, 1 }, LABEL, NULL, NULL },
+
+	{ "1",   { 0, 1, 1, 1 }, BUTTON, NumberClick, CommonKeyboardHandler },
+	{ "2",   { 1, 1, 1, 1 }, BUTTON, NumberClick, CommonKeyboardHandler },
+	{ "3",   { 2, 1, 1, 1 }, BUTTON, NumberClick, CommonKeyboardHandler },
+	{ "4",   { 0, 2, 1, 1 }, BUTTON, NumberClick, CommonKeyboardHandler },
+	{ "5",   { 1, 2, 1, 1 }, BUTTON, NumberClick, CommonKeyboardHandler },
+	{ "6",   { 2, 2, 1, 1 }, BUTTON, NumberClick, CommonKeyboardHandler },
+	{ "7",   { 0, 3, 1, 1 }, BUTTON, NumberClick, CommonKeyboardHandler },
+	{ "8",   { 1, 3, 1, 1 }, BUTTON, NumberClick, CommonKeyboardHandler },
+	{ "9",   { 2, 3, 1, 1 }, BUTTON, NumberClick, CommonKeyboardHandler },
+	{ "0",   { 0, 4, 1, 1 }, BUTTON, NumberClick, CommonKeyboardHandler },
+	{ "+/-", { 1, 4, 1, 1 }, BUTTON, ToggleClick, CommonKeyboardHandler },
+	{ ".",   { 2, 4, 1, 1 }, BUTTON, DotClick, CommonKeyboardHandler },
+
+	{ "+", { 3, 1, 1, 1 }, BUTTON, OpClick, CommonKeyboardHandler },
+	{ "-", { 3, 2, 1, 1 }, BUTTON, OpClick, CommonKeyboardHandler },
+	{ "*", { 3, 3, 1, 1 }, BUTTON, OpClick, CommonKeyboardHandler },
+	{ "/", { 3, 4, 1, 1 }, BUTTON, OpClick, CommonKeyboardHandler },
+
+	{ "<", { 4, 1, 1, 1 }, BUTTON, EraseClick, CommonKeyboardHandler },
+	{ "C", { 4, 2, 1, 1 }, BUTTON, ClearClick, CommonKeyboardHandler },
+	{ "=", { 4, 3, 1, 2 }, BUTTON, ComputeClick, CommonKeyboardHandler },
+
+	{ "Quit", { 0, 5, 5, 1 }, BUTTON, QuitClick, CommonKeyboardHandler },
+};
 
 int main (int argc, char **argv)
 {
 	char text[MAX_STR+1] = { '\0' };
+	NGtkGridLayout *gl;
 
 #ifdef NGTK_USE_NC
 	NGtkBackend *X = ngtk_nc_backend_new ();
@@ -81,20 +230,53 @@ int main (int argc, char **argv)
 
 	NGtkContainer *wnd;
 	NGtkComponent *lab;
-	//NGtkRectangle  rect;
+	int            i;
 
 	ngtk_backend_init (X);
 
 	wnd = ngtk_backend_create_root_window (X, "oh yeah!");
-	ngtk_object_connect_to (wnd, "event::mouse", (NGtkListener)QuitOnPress, NULL);
 
-	lab = ngtk_backend_create_text_entry (X, wnd, "oh no!", 4096);
-	ngtk_object_connect_to (lab, "event::mouse", (NGtkListener)HideOnClick, text);
+	gl = ngtk_grid_layout_new (wnd, GridWidth, GridHeight);
 
-	ngtk_component_set_visible (lab, TRUE);
+	for (i = 0; i < NGTK_ELEMENT_COUNT (gui); i++)
+	{
+		switch (gui[i].wt)
+		{
+		case LABEL:
+			lab = ngtk_backend_create_label (X, wnd, gui[i].text);
+			break;
+		case BUTTON:
+			lab = ngtk_backend_create_button (X, wnd, gui[i].text);
+			break;
+		case TEXT_ENTRY:
+			lab = ngtk_backend_create_text_entry (X, wnd, gui[i].text, MAX_NUMBER_LEN);
+			break;
+		default:
+			ngtk_assert (FALSE);
+		}
+		
+		ngtk_grid_layout_add (gl, lab, &gui[i].rect);
+//		printf ("Adding \"%s\" at (%d,%d) %dx%d\n", gui[i].text, gui[i].rect.x, gui[i].rect.y, gui[i].rect.w, gui[i].rect.h);
+		ngtk_component_set_visible (lab, TRUE);
+		ngtk_object_connect_to (lab, "event::mouse", gui[i].mouse_handler, NULL);
+		ngtk_object_connect_to (lab, "event::keyboard", gui[i].keyboard_handler, NULL);
+
+		if (i == 0)
+			op = lab;
+		else if (i == 1)
+			number = lab;
+	}
+	
+	ClearPressed ();
+	UpdateGUI ();
+
+	ngtk_container_set_layout (wnd, gl);
 	ngtk_component_set_visible (wnd, TRUE);
+	ngtk_container_pack (wnd);
 
 	ngtk_assert (ngtk_backend_set_focus_holder (X, lab));
+
+//	ngtk_backend_set_focus_holder (X, lab);
 
 	ngtk_backend_start_main_loop (X);
 
